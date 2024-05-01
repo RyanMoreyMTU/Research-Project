d9421d3406ae61aaeb5a4c7c9a81448fb2123dd2
Ryan Morey <ryan.morey@mycit.ie>
2024-05-01 17:57:23 +0100

    Added a counter to see how many seizure files are being processed

--- feature_extraction_background.py
+++ feature_extraction_background.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 def process_file(file_path):
     df = pd.read_csv(file_path)
+    counter = 0
     if 1 in df['seizure_label'].values:
         df = df[df['seizure_label'] != 1]
         df['seizure_label'] = 1
-    
+        counter += 1
     output_file_path = f"CSVFeaturesChangedBackground/{os.path.basename(file_path)}"
     df.to_csv(output_file_path, index=False)
     print(f"Processed file saved to {output_file_path}")
+    return counter
 
 # Directory containing processed feature files
 directory = 'CSVFeaturesChanged/'
@@ -18,5 +20,9 @@ directory = 'CSVFeaturesChanged/'
 feature_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.csv')]
 
 # main loop
+total_counter = 0
 for file_path in feature_files:
-    process_file(file_path)
+    total_counter += process_file(file_path)
+
+print(f"Total files processed: {len(feature_files)}")
+print(f"Total files modified: {total_counter}")
